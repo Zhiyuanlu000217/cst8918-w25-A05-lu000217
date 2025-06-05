97727c80a6e30c9f9e052f591c327ed5df6a7bf3
Zhiyuanlu000217 <lu000217@algonquinlive.com>
2025-06-05 18:42:57 -0400

    chore: modify output value

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "resource_group_name" {
   description = "The name of the resource group"
-  value       = azurerm_resource_group.main.name
+  value       = azurerm_resource_group.resource_group.name
 }
 
 output "public_ip_address" {
   description = "The public IP address of the web server"
-  value       = azurerm_public_ip.main.ip_address
+  value       = azurerm_public_ip.public_ip.ip_address
 }
